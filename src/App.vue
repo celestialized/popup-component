741e028bb4f83cf4b8acb646fc283331b69f8042
sirricc <patrick-pesch@hotmail.de>
2020-08-24 12:39:04 +0200

    CHANGED: moved data to components since a child cannot alter data from a parent

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="app">
-  <Popup v-bind:pills="tags" />
+  <Popup />
 </div>
 </template>
 
@@ -9,31 +9,6 @@ import Popup from './components/Popup.vue'
 
 export default {
   name: 'App',
-  data() {
-    return {
-      tags: [{
-          id: 0,
-          name: "tree"
-        },
-        {
-          id: 1,
-          name: "banana"
-        },
-        {
-          id: 2,
-          name: "obama"
-        },
-        {
-          id: 3,
-          name: "karma"
-        },
-        {
-          id: 4,
-          name: "wall"
-        },
-      ]
-    }
-  },
   components: {
     Popup
   }
