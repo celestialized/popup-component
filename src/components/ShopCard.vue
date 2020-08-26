926740ee5bd2594ff65280bf135b45cb311433e1
sirricc <patrick-pesch@hotmail.de>
2020-08-26 17:11:22 +0200

    CHANGED: layout

--- src/components/ShopCard.vue
+++ src/components/ShopCard.vue
@@ -1,30 +1,39 @@
 <template lang="html">
-  <div class="container">
-    <div class="card">
-    <img src="../assets/logo.png" alt="img">
-    <hr>
-    <h2>Product name</h2>
+  <div class="card">
+
   </div>
-</div>
 </template>
 
 <script>
+// import ProductCard from './ProductCard.vue'
+
 export default {
   name: 'ShopCard',
+  components: {
+    // ProductCard
+  },
+  data() {
+    return {
+      productName: "Shoe",
+      productId: "01001011",
+      productDesc: "This is very much very nice",
+      productPrice: "twofiddy",
+      available: false
+    }
+  },
 }
 </script>
 
 <style lang="css" scoped>
-  .card {
-    display: flex;
-    border: 1px solid grey;
-    margin: 3px;
-    border-radius: 3px;
-    text-align: center;
-  }
-  .card > img {
-    margin: 25px;
-    border: 1px solid grey;
 
-  }
+.card {
+  height: 200px;
+  width: 175px;
+  border: 2px solid rgba(1, 1, 1, .1);
+  border-radius: 4px;
+  margin: 10px;
+  box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.15);
+}
+
+
 </style>
