7bf0e37251a7d1de130d7acb4ddf05bbdaf21cf3
sirricc <patrick-pesch@hotmail.de>
2020-08-31 14:03:01 +0200

    ADDED: basic popup is working due to dom manipulation.

--- src/components/ShopCard.vue
+++ src/components/ShopCard.vue
@@ -1,5 +1,6 @@
 <template lang="html">
-  <div :class="[toggle.maxed?'cardMax':'card']" v-on:click="toggle.maxed = !toggle.maxed">
+  <div id="root" :class="[toggle.maxed?'cardMax':'card']" v-on:click="popUp">
+  <!-- <div  :class="[toggle.maxed?'cardMax mx-auto':'card']" v-on:click="toggle.maxed = !toggle.maxed"> -->
     <p> {{ content.Title }}</p>
     <p> {{ content.ImageSrc }}</p>
     <p> {{ content.VariantPrice }}</p>
@@ -13,6 +14,9 @@
 
 export default {
   name: 'ShopCard',
+  components: {
+    // ProductCard
+  },
   props: {
     // TODO: process passed props -> computed
   },
@@ -31,11 +35,38 @@ export default {
         maxed: false
       }
     }
+  },
+  methods: {
+    popUp() {
+      if (this.toggle.maxed == false) {
+        this.toggle.maxed = !this.toggle.maxed
+        // alert("false");
+        document.body.insertBefore(  document.getElementById( 'root'), document.body.firstChild  );
+        document.getElementById( 'root').classList.add("target");
+      } else {
+        this.toggle.maxed = !this.toggle.maxed
+        // alert("true");
+        // document.body.removeChild( document.getElementByClassName("target"));
+        document.body.removeChild( document.body.children[0] )
+        document.body.insertBefore(document.getElementById( 'root'), document.getElementByClassName("products").firstChild);
+      }
+
+
+    }
   }
 }
   </script>
 
 <style lang="css" scoped>
+
+/* .container123123 {
+  position: absolute;
+  top: 50px;
+  left: 50px;
+  border: 2px solid red;
+  height: 100px;
+  width: 100px;
+} */
 
 .card {
   background-color: white;
@@ -49,14 +80,16 @@ export default {
 }
 
 .cardMax {
+
   position: absolute;
-  left: 10vh;
-  right: 10vh;
+  /* left: 50%;
+  top: 10vh; */
+  min-width: 150px;
   background-color: white;
   z-index: 2;
   cursor: pointer;
-  height: 100vh;
-  width: 100vh;
+  height: 50vh;
+  width: 50vh;
   margin: auto;
   border: 2px solid red;
   border-radius: 4px;
