6e67bda3bd6d6342ccc822816ea2799ddc5d31fb
sirricc <patrick-pesch@hotmail.de>
2020-09-07 19:19:50 +0200

    PROBLEM: popup takes parent small product card a position to render the max product card
    NEW APPROACH: hide popup template in parent and render when child 
    changes

--- src/components/ShopCard.vue
+++ src/components/ShopCard.vue
@@ -1,6 +1,28 @@
 <template lang="html">
-  <div id="root" :class="[toggle.maxed?'cardMax':'card']" v-on:click="popUp">
-    <p v-bind="content"> {{ content.Title }} </p>
+  <!-- <div id="root" :class="[toggle.maxed?'cardMax':'card']" v-on:click="popUp"> -->
+  <div id="root" class="card">
+
+    <!-- CONTENT -->
+    <!-- TODO: WORK HERE -->
+    <img :src="content.ImageSrc" alt="productImg">
+    <p> {{ content.Title }} <p>
+    <p > {{ content.VariantPrice }} $</p>
+    <button @click="showModal = true">Show details</button>
+
+    <!-- MODAL_OVERLAY -->
+    <div class="modal-overlay" v-if="showModal" @click="showModal = false">
+
+      <!-- MODAL -->
+      <div class="modal" v-if="showModal">
+       <h1>Lorem Ipsum</h1>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem provident explicabo accusamus laudantium voluptatum nobis sed nesciunt neque possimus molestiae?</p>
+       <button class="button" @click="showModal = true">
+        Close Modal
+       </button>
+      </div>
+    </div>
+
+
   </div>
 </template>
 
@@ -12,36 +34,20 @@ export default {
   props: ["content"],
   data() {
     return {
-      toggle: {
-        maxed: false
-      }
+      showModal: false
     }
   },
-  methods: {
-    popUp() {
-      if (this.toggle.maxed == false) {
-        this.toggle.maxed = !this.toggle.maxed
-        // alert("false");
-        document.body.insertBefore(document.getElementById('root'), document.body.firstChild);
-        // TODO: this only gets the first element and attaches "target",  use id as arg in this function
-        document.getElementById('root').classList.add("target");
-      } else {
-        this.toggle.maxed = !this.toggle.maxed
-        // alert("true");
-        // document.body.removeChild( document.getElementByClassName("target"));
-        document.body.removeChild(document.body.children[0])
-        document.body.insertBefore(document.getElementById('root'), document.getElementByClassName("products").firstChild);
-      }
-    }
-  }
+  methods: {}
 }
 </script>
 
 <style lang="css" scoped>
 .card {
+  display: flex;
+  justify-content: space-between;
   background-color: white;
   cursor: pointer;
-  height: 250px;
+  max-height: 250px;
   width: 200px;
   border: 1px solid rgba(1, 1, 1, .1);
   border-radius: 0px;
@@ -49,19 +55,48 @@ export default {
   box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.15);
 }
 
-.cardMax {
-  position: absolute;
-  min-width: 150px;
-  background-color: white;
-  z-index: 2;
-  cursor: pointer;
-  height: 50vh;
-  width: 50vh;
-  margin: auto;
-  border: 2px solid red;
-  border-radius: 4px;
-  margin: 10px;
-  box-shadow: 0px 5px 5px rgba(3, 23, 18, 0.15);
+.card > img {
+  height: 125px;
+}
+
+.card > h6 {
+  margin-top: 15px;
+}
+
+button {
+  /* display: inline-block; */
+  padding: 5px 5px;
+  background-color: #41B883;
+  border-radius: 0px;
+  color: #FFF;
+  font-size: 18px;
+  font-weight: 500;
+  border: none;
+}
+
+.modal-overlay {
+ position: fixed;
+ width: 90%;
+ height: 80%;
+ top: 10%;
+ margin: auto;
+ z-index: 2;
+ background-color: rgba(0, 0, 0, 0.3);
+ border: 3px solid red;
+}
+
+.modal {
+ position: fixed;
+ top: 50%;
+ left: 50%;
+ /* transform: translate(-50%, -50%); */
+ z-index: 3;
+ width: 100;
+ max-width: 400px;
+ background-color: #FFF;
+ padding: 25px;
+ border: 5px solid green;
+
 }
 
 @media only screen and (max-width: 600px) {
